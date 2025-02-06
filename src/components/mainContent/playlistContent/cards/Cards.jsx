0e821558b4a81ba003f3a698283d3fcf2fc3cfef
Douglas Araujo <douglasdab10@gmail.com>
2025-02-06 17:54:02 -0300

    Update Cards.jsx

--- src/components/mainContent/playlistContent/cards/Cards.jsx
+++ src/components/mainContent/playlistContent/cards/Cards.jsx
@@ -5,6 +5,21 @@ import {
   Postcards,
   CardSpan,
 } from "./Cards.styled";
+import imgBoasFestas from "../../../../assets/playlist/1.jpeg";
+import imgFeitosParaVoce from "../../../../assets/playlist/2.png";
+import imgLancamentos from "../../../../assets/playlist/3.jpeg";
+import imgCreators from "../../../../assets/playlist/4.jpeg";
+import imgParaTreinar from "../../../../assets/playlist/5.jpeg";
+import imgPodcasts from "../../../../assets/playlist/6.jpeg";
+import imgSertanejo from "../../../../assets/playlist/7.jpeg";
+import imgSambaEPagode from "../../../../assets/playlist/8.jpeg";
+import imgFunk from "../../../../assets/playlist/9.jpeg";
+import imgMPB from "../../../../assets/playlist/10.jpeg";
+import imgRock from "../../../../assets/playlist/11.jpeg";
+import imgHipHop from "../../../../assets/playlist/12.jpeg";
+import imgIndie from "../../../../assets/playlist/13.jpeg";
+import imgRelax from "../../../../assets/playlist/14.jpeg";
+import imgMusicaLatina from "../../../../assets/playlist/15.jpeg";
 
 const Card = ({ imageSrc, text, background, href }) => (
   <Postcards href={href} className="cards" style={{ background }}>
@@ -16,91 +31,91 @@ const Card = ({ imageSrc, text, background, href }) => (
 export default function Cards() {
   const cardsData = [
     {
-      imageSrc: "../../../..//assets/playlist/1.jepg",
+      imageSrc: imgBoasFestas,
       text: "Boas festas",
       background: "rgb(0, 100, 80)",
       href: "https://example.com/boas-festas",
     },
     {
-      imageSrc: "../../../../assets/playlist/2.png",
+      imageSrc: imgFeitosParaVoce,
       text: "Feitos para você",
       background: "rgb(132, 0, 231)",
       href: "https://example.com/feitos-para-voce",
     },
     {
-      imageSrc: "../../../../assets/playlist/3.jpeg",
+      imageSrc: imgLancamentos,
       text: "Lançamentos",
       background: "rgb(30, 50, 100)",
       href: "https://example.com/lancamentos",
     },
     {
-      imageSrc: "../../../../src/assets/playlist/4.jpeg",
+      imageSrc: imgCreators,
       text: "Creators",
       background: "rgb(140, 25, 50)",
       href: "https://example.com/creators",
     },
     {
-      imageSrc: "../../../../assets/playlist/5.jpeg",
+      imageSrc: imgParaTreinar,
       text: "Para treinar",
       background: "rgb(232, 17, 91)",
       href: "https://example.com/para-treinar",
     },
     {
-      imageSrc: "../../../../assets/playlist/6.jpeg",
+      imageSrc: imgPodcasts,
       text: "Podcasts",
       background: "rgb(83, 122, 161)",
       href: "https://example.com/podcasts",
     },
     {
-      imageSrc: "../../../../assets/playlist/7.jpeg",
+      imageSrc: imgSertanejo,
       text: "Sertanejo",
       background: "rgb(142, 102, 172)",
       href: "https://example.com/sertanejo",
     },
     {
-      imageSrc: "../../../../assets/playlist/8.jpeg",
+      imageSrc: imgSambaEPagode,
       text: "Samba e pagode",
       background: "rgb(20, 138, 8)",
       href: "https://example.com/samba-e-pagode",
     },
     {
-      imageSrc: "../../../../assets/playlist/9.jpeg",
+      imageSrc: imgFunk,
       text: "Funk",
       background: "rgb(30, 50, 100)",
       href: "https://example.com/funk",
     },
     {
-      imageSrc: "../../../../assets/playlist/10.jpeg",
+      imageSrc: imgMPB,
       text: "MPB",
       background: "rgb(233, 20, 41)",
       href: "https://example.com/mpb",
     },
     {
-      imageSrc: "../../../../assets/playlist/11.jpeg",
+      imageSrc: imgRock,
       text: "Rock",
       background: "rgb(80, 55, 80)",
       href: "https://example.com/rock",
     },
     {
-      imageSrc: "../../../../assets/playlist/12.jpeg",
+      imageSrc: imgHipHop,
       text: "Hip Hop",
       background: "rgb(216, 64, 0)",
       href: "https://example.com/hip-hop",
     },
     {
-      imageSrc: "../../../../assets/playlist/13.jpeg",
+      imageSrc: imgIndie,
       text: "Indie",
       background: "rgb(186, 93, 7)",
       href: "https://example.com/indie",
     },
     {
-      imageSrc: "../../../../assets/playlist/14.jpeg",
+      imageSrc: imgRelax,
       text: "Relax",
       background: "rgb(0, 30, 80)",
       href: "https://example.com/relax",
     },
     {
-      imageSrc: "../../../../assets/playlist/15.jpeg",
+      imageSrc: imgMusicaLatina,
       text: "Música Latina",
       background: "rgb(60, 30, 80)",
       href: "https://example.com/musica-latina",
