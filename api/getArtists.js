2a0b68539b5c1f4f4e0534555fc463b60a875e41
Douglas Araujo <douglasdab10@gmail.com>
2025-02-06 19:36:48 -0300

    Update getArtists.js

--- api/getArtists.js
+++ api/getArtists.js
@@ -1,25 +1,25 @@
-import fs from 'fs';
-import path from 'path';
+// api/getArtists.js
 
 export default function handler(req, res) {
-  // Caminho para o arquivo artists.json na pasta 'data'
-  const filePath = path.join(process.cwd(), 'data', 'artists.json');
-  // Ler o conteúdo do arquivo
-  const jsonData = fs.readFileSync(filePath, 'utf8');
-  const artistsData = JSON.parse(jsonData);
+  try {
+    const artistsData = require('./artists.json');
 
-  if (req.method === 'GET') {
-    const { name_like } = req.query;
+    if (req.method === 'GET') {
+      const { name_like } = req.query;
 
-    // Filtrar artistas com base no parâmetro de busca, se fornecido
-    const filteredArtists = name_like
-      ? artistsData.artists.filter((artist) =>
-          artist.name.toLowerCase().includes(name_like.toLowerCase())
-        )
-      : artistsData.artists;
+      // Filtrar artistas com base no parâmetro de busca, se fornecido
+      const filteredArtists = name_like
+        ? artistsData.artists.filter((artist) =>
+            artist.name.toLowerCase().includes(name_like.toLowerCase())
+          )
+        : artistsData.artists;
 
-    res.status(200).json(filteredArtists);
-  } else {
-    res.status(405).json({ message: 'Método não permitido' });
+      res.status(200).json(filteredArtists);
+    } else {
+      res.status(405).json({ message: 'Método não permitido' });
+    }
+  } catch (error) {
+    console.error('Erro no handler:', error);
+    res.status(500).json({ message: 'Erro no servidor' });
   }
 }
